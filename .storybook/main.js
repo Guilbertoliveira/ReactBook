2127407e3b3daea4943d4852f5b1b18e1b2cdb0c
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2024-07-14 11:50:59 -0300

    chore: storybook installation

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  staticDirs: ['..\\public'],
+};
+export default config;
